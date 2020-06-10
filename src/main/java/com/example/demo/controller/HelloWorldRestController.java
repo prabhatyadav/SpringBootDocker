e39717f25254efe583ee9f7158dc385741ba86d8
Prabhat <prabhatyadav03@gmail.com>
2020-06-10 23:31:00 +0530

    basic spring application

---
+++ src/main/java/com/example/demo/controller/HelloWorldRestController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/rest")
+public class HelloWorldRestController {
+
+	@RequestMapping(path="/helloWorld",method=RequestMethod.GET)
+	public String  sayHelloWorld() {
+	 return "Hello World";	
+	}
+	
+	@RequestMapping(path="/hello/{name}",method=RequestMethod.GET)
+	public String  sayHelloPathVariable(@PathVariable("name") String subject) {
+	 return "Hello "+subject;	
+	}
+	
+	@RequestMapping(path="/hello",method=RequestMethod.GET)
+	public String  sayHelloQueryParameter(@RequestParam("name") String name) {
+	 return "Hello "+name;	
+	}
+	
+}
