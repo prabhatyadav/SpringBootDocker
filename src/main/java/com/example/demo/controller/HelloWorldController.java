5410454265c7d2b47a142c96f55d5de9c9add7ce
Prabhat <prabhatyadav03@gmail.com>
2020-07-19 22:32:49 +0530

    dockerfile at parellel to src folder index.html file added

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldController {
+	
+	@RequestMapping(path="/index", method=RequestMethod.GET)
+	 public String getIndexFile(Model model ) {
+		 return "index";
+	 }
 
 }
--- src/main/java/com/example/demo/controller/HelloWorldRestController.java
+++ src/main/java/com/example/demo/controller/HelloWorldRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/rest")
+@RestController
 public class HelloWorldRestController {
 
 	@RequestMapping(path="/helloWorld",method=RequestMethod.GET)
